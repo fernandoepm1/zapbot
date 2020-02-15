f0c0d17858c10fc01558164936e7a0d36a8f0cd3
Fernando Eufrásio <fernando.epm1@gmail.com>
2020-02-15 19:55:38 -0300

    Add open_chat method for selecting a contact

--- zapbot.py
+++ zapbot.py
@@ -14,4 +14,16 @@ class Zapbot:
     self.driver.get("https://web.whatsapp.com/")
     self.driver.implicitly_wait(15)
 
+  def open_chat(self, contact):
+    try:
+      self.search_box = self.driver.find_element_by_class_name("jN-F5")
+      self.search_box.send_keys(contact)
+      sleep(2)
+
+      self.contact = self.driver.find_element_by_xpath("//span[@title = '{}']".format(contact))
+      self.contact.click()
+      sleep(2)
+    except Exception as e:
+      print("Chat could not be opened", e)
+
 bot = Zapbot()
